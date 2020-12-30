79cff7739a1d4329026b916841c852b5259e5e68
Alan Baldwin <baldwingaming1979@gmail.com>
2020-12-30 18:28:54 +0000

    Added absolute imports path

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { shallow, mount, render } from "enzyme";
-import App from "../App";
-import CommentBox from "../CommentBox";
-import CommentList from "../CommentList";
+import App from "components/App";
+import CommentBox from "components/CommentBox";
+import CommentList from "components/CommentList";
 
 it("displays a comment box", () => {
   const wrapped = shallow(<App />);
   expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it("displays a comment list", () => {
+  const wrapped = shallow(<App />);
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });
